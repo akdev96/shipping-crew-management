9e359b257e56bd5cf81c86837ff91486b4d8c9e8
Dilan Fernando <spegusess@gmail.com>
2022-10-09 23:46:18 +0530

    Updated components

--- models/profiles.js
+++ models/profiles.js
@@ -1,17 +1,62 @@
 const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  profileCategory: {
+    type: String,
+    required: true,
+  },
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  regNo: {
+    type: String,
+    required: true,
+  },
+  availabilityStatus: {
+    type: String,
+    default: "Available",
+  },
+  grade: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+  },
+  profilePic: {
+    type: String,
+  },
+  log: [
+    {
+      description: String,
+      date: Date,
     },
-    bio:{
-        type: String,
-        required: true
-    },
-    profileCategory:{
-        type: String,
-        required: true
-    }
+  ],
+  isActive: {
+    type: Boolean,
+  },
 });
 
 module.exports = mongoose.model('Profiles', profileSchema);
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,68 +1,87 @@
 const express = require('express');
-const Profiles = require('../models/profiles');
+const profiles = require("../models/profiles");
+const Profiles = require("../models/profiles");
 
 const router = express.Router();
 
 // Saving Profiles
-router.post('/profiles/save', (req, res) => {
-    let newProfile = new Profiles(req.body);
-    newProfile.save((err) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            });
-        }
-        return res.status(200).json({
-            success: "Profiles Saved Successfully"
-        });
+router.post("/profiles/save", (req, res) => {
+  let newProfile = new Profiles(req.body);
+  newProfile.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: "Profiles Saved Successfully",
     });
+  });
 });
 
 // Retrieving Profiles
-router.get('/profiles', (req, res) => {
-    Profiles.find().exec((err, profiles) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            });
-        }
-        return res.status(200).json({
-            success:true,
-            existingProfiles: profiles
-        });
+router.get("/profiles", (req, res) => {
+  Profiles.find().exec((err, profiles) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingProfiles: profiles,
     });
+  });
 });
 
 // Update Profiles
-router.put('/profile/update/:id', (req, res) => {
-    Profiles.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: req.body
-        },
-        (err, profile) =>{
-            if(err){
-                return res.status(400).json({error: err});
-            }
-            return res.status(200).json({
-                success: "Updated Successfully"
-            });
-        }
-    );
+router.put("/profile/update/:id", (req, res) => {
+  Profiles.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, profile) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      return res.status(200).json({
+        success: "Updated Successfully",
+      });
+    }
+  );
 });
 
 // Delete Profile
-router.delete('/profile/delete/:id', (req, res) => {
-    Profiles.findByIdAndRemove(req.params.id).exec((err, deletedProfile) => {
-        if(err){
-            return res.status(400).json({
-                message: "Delete Unsuccessfull"
-            });
-        }
-        return res.json({
-            message: "Deleted Successfully", deletedProfile
-        });
+router.delete("/profile/delete/:id", (req, res) => {
+  Profiles.findByIdAndRemove(req.params.id).exec((err, deletedProfile) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Delete Unsuccessfull",
+      });
+    }
+    return res.json({
+      message: "Deleted Successfully",
+      deletedProfile,
     });
+  });
+});
+
+// Retrieve a specific profile
+router.get("/profile/:id", (req, res) => {
+  let postId = req.params.id;
+  Profiles.findById(postId, (err, profile) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      profile,
+    });
+  });
 });
 
 module.exports = router;
